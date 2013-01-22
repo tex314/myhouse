0c81049d9f3f633415d4a2b22dc9e80d58c14ab9
OKAZAKI Takurou <okazaki@isola.co.jp>
2013-01-22 23:45:32 +0900

    Initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+```ruby
+$ rvm use ruby-1.9.3-head@myhouse --create
+$ gem install bundler
+$ gem install rails
+$ rails new myhouse -T --skip-bundle
+$ cd myhouse
+$ git init .
+$ git flow init
+$ git add .
+$ git commit
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myhouse::Application.config.secret_token = '8e20a09eeecbab1b0531574db2e5661846f9740a55305c758bc7918a305edbb65788c84f9c2abf81c3e09b0a463eba1ec8349b82c9b0a43be9e236fbe5d69bd4'
